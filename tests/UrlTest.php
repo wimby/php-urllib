dd8d9e87913b1abe471fe99a7681ea234caa4542
Richard Wimberský <rw@Richards-MacBook-Pro.local>
2018-10-21 21:32:35 +0200

    add initial test

---
+++ tests/UrlTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Wimby\Url\Url;
+
+
+class UrlTest extends TestCase
+{
+    function testOne()
+    {
+        $url = new Url();
+        $this->assertInstanceOf(Url::class, $url);
+    }
+
+    function testTwo()
+    {
+        $this->assertEquals(true, false);
+    }
+}
